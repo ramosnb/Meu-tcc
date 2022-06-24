6e121672fb59138d8e554917366e76d057c44d46
Guilherme Scherner <70420496+ramosnb@users.noreply.github.com>
2022-06-23 22:37:21 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
