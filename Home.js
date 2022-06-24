90697653e8d686fb7d21ef962a7f5dc62b45f29e
Guilherme Scherner <70420496+ramosnb@users.noreply.github.com>
2022-06-23 22:35:43 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -10,7 +10,7 @@ export default function Home(props)
         
         <View style={styles.container}>
             
-            <Image source={require('../Image/carro.png')}
+            <Image source={require('../carro.png')}
                style= {{width: 250, height: 250}}>
             </Image>
             <TouchableHighlight 
